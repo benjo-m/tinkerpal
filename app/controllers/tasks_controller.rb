ca13137243296bb8da17fb75e48bf07ebff3eeae
Benjo <maglicbenjamin@gmail.com>
2025-05-01 01:21:06 +0200

    routing and security fixes, style changes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,11 +3,11 @@ class TasksController < ApplicationController
 
   # GET /tasks or /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = Task.all.order(created_at: :desc)
     @cities = City.all
   end
 
-  # GET /tasks/1 or /tasks/1.json
+  # GET /tasks/1
   def show
   end
 
@@ -19,32 +19,32 @@ class TasksController < ApplicationController
   # GET /tasks/1/edit
   def edit
     if @task.user != Current.user
-      redirect_to tasks_path
+      redirect_to root_path
     end
   end
 
-  # POST /tasks or /tasks.json
+  # POST /tasks
   def create
     # @task = Task.new(task_params)
     # @task.user = Current.user
     @task = Current.user.tasks.build(task_params) # kraca verzija ovog iznad
     if @task.save
-      redirect_to @task
+      redirect_to params[:return_to].presence || root_path
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /tasks/1 or /tasks/1.json
+  # PATCH/PUT /tasks/1
   def update
       if @task.update(task_params)
-        redirect_to my_task_path(@task)
+        redirect_to profile_path
       else
         render :edit, status: :unprocessable_entity
       end
   end
 
-  # DELETE /tasks/1 or /tasks/1.json
+  # DELETE /tasks/1
   def destroy
     if @task.user == Current.user
       @task.destroy!
@@ -54,21 +54,13 @@ class TasksController < ApplicationController
     end
   end
 
-  def user_task
-    @task = Task.find(params.expect(:task_id))
-  end
-
-  def my_task
-    set_task
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_task
-      @task = Task.find(params.expect(:id))
+      @task = Task.find_by(id: params.expect(:id))
+      redirect_to root_path if @task.nil?
     end
 
-    # Only allow a list of trusted parameters through.
     def task_params
       params.expect(task: [ :title, :description, :city_id ])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,7 @@ Rails.application.routes.draw do
   resources :passwords, param: :token
   resources :users
 
-  get "users/:user_id/tasks/:task_id", to: "tasks#user_task", as: "user_task"
   get "profile", to: "users#profile"
-  get "profile/tasks/:id", to: "tasks#my_task", as: "my_task"
-  get "profile/tasks/:id/edit", to: "tasks#edit", as: "edit"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
