2825c157d2371de9ee882eea61d056c363aa295d
Benjo <maglicbenjamin@gmail.com>
2025-05-07 20:03:01 +0200

    about me section now editable. style changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.expect(user: [ :username, :email_address, :password, :city ])
+    params.expect(user: [ :username, :email_address, :password, :city, :about_me ])
   end
 end
