d8b23f754fbcb8ca1786a1af9bc79f4950ea3a40
Benjo <maglicbenjamin@gmail.com>
2025-05-11 22:27:56 +0200

    offer form validation

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,17 @@
 class Offer < ApplicationRecord
-  validates :price, presence: true
+  validates :price, :time, :date, presence: true
   belongs_to :task
   belongs_to :user
+  validate :date_and_time_cannot_be_in_the_past
+
+  def date_and_time_cannot_be_in_the_past
+    return if date.blank? || time.blank?
+
+    offer_datetime = DateTime.new(date.year, date.month, date.day, time.hour, time.min)
+    current_datetime = Time.current
+
+    if offer_datetime < current_datetime
+      errors.add(:date_and_time, "must be in the future.")
+    end
+  end
 end
