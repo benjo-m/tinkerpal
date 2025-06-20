4d6358ab0b8c8ed82fb380b2f6189a07e90db616
Benjo <maglicbenjamin@gmail.com>
2025-06-20 21:12:21 +0200

    add email validation and min/max password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true, uniqueness: true, length: { maximum: 50 }
-  validates :email_address, presence: true, uniqueness: true
+  validates :password, length: { in: 6..100 }
+  validates :email_address, presence: true, uniqueness: true, format: URI::MailTo::EMAIL_REGEXP
   has_many :sessions, dependent: :destroy
   has_many :tasks, dependent: :destroy
   belongs_to :city
